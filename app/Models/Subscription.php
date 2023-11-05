19f07175b63c048ff9fdc99a36e3f8152f728698
Mohammad Shahriari Fard <unshields@gmail.com>
2023-11-05 20:06:31 +0330

    feat: add email template

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Subscription extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    protected $fillable = ['user_id', 'website_id', 'expired_at'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function website()
+    {
+        return $this->belongsTo(Website::class, 'website_id');
+    }
 }
---
+++ resources/views/emails/new_post.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>New Post Published</title>
+</head>
+<body>
+<h1>New Post Published</h1>
+<p>
+    A new post has been published:
+    <strong>{{ $title }}</strong>
+</p>
+<p>{{ $content }}</p>
+</body>
+</html>
